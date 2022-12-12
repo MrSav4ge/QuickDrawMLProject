78cd6c420c0ecbc6a58eecffe79431a578e63547
Jake Landers <jake@landersweb.com>
2022-12-12 15:43:05 -0800

    some updates

---
+++ show_img.py
@@ -0,0 +1,25 @@
+import json
+from PIL import Image, ImageDraw
+
+# read ndjson lines
+lines = open(
+    "/Users/jakelanders/code/QuickDrawMLProject/full-simplified-airplane.ndjson", "r"
+).readlines()
+# grab the first line, JSON parse it and fetch the 'drawing' array
+raw_drawing = json.loads(lines[100])["drawing"]
+
+print("before", raw_drawing)
+# zip x,y coordinates for each point in every polyline
+polylines = (list(zip(polyline[0], polyline[1])) for polyline in raw_drawing)
+# notice how the data is shuffled to (x1,y1),(x2,y2) order
+print("after", polylines)
+
+# make a new image
+pil_img = Image.new("RGB", (240, 270), (255, 255, 255))
+# get a drawing context
+d = ImageDraw.Draw(pil_img)
+# render each polyline
+for polyline in polylines:
+    d.line(polyline, fill=(0, 0, 0), width=3)
+# display image
+pil_img.show()
